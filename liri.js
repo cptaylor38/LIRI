17377de187c743a38867c3d07c4b8de56e33b594
Corbin Taylor <cptaylor38@yahoo.com>
2019-04-04 02:19:06 -0400

    fully functioning

--- liri.js
+++ liri.js
@@ -16,23 +16,23 @@ switch (request) {
         if (!userQuery) {
             userQuery = 'The Sign';
         }
-        songSearch();
+        songSearch(userQuery);
         break;
     case 'movie-this':
         if (!userQuery) {
             userQuery = 'Mr. Nobody';
         }
-        movieSearch();
+        movieSearch(userQuery);
         break;
     case 'concert-this':
-        bandSearch();
+        bandSearch(userQuery);
         break;
     case 'do-what-it-says':
-        readRandom();
+        readRandom(userQuery);
         break;
 }
 
-function songSearch() {
+function songSearch(userQuery) {
     console.log('function reached');
     spotify.search({ type: 'track', query: '' + userQuery }, function (err, data) {
         if (err) {
@@ -43,12 +43,12 @@ function songSearch() {
 
             for (var i = 0; i < dataItems.length; i++) {
 
-                dataString = `
-                Artist:         ${dataItems[i].artists[0].name}
-                Song Title:     ${dataItems[i].name}
-                Song Link       ${dataItems[i].preview_url}
-                Album:          ${dataItems[i].album.name}
-                -------------------------------------------------------------
+                var dataString = `
+Artist:         ${dataItems[i].artists[0].name}
+Song Title:     ${dataItems[i].name}
+Song Link       ${dataItems[i].preview_url}
+Album:          ${dataItems[i].album.name}
+-------------------------------------------------------------
                 `
                 console.log(dataString);
                 fs.appendFile("log.txt", dataString, function (err) {
@@ -57,20 +57,14 @@ function songSearch() {
                         console.log(err);
                     }
 
-                    else {
-                        console.log("Content Added!");
-                    }
-
                 });
 
             }
-
-
         }
     });
 }
 
-function movieSearch() {
+function movieSearch(userQuery) {
     console.log('movieSearch function ran');
     axios.get(`http://www.omdbapi.com/?t=${userQuery}&y=&plot=short&apikey=trilogy`).then(
         function (response) {
@@ -80,15 +74,15 @@ function movieSearch() {
             else {
 
                 var dataString = `
-            Title:      ${response.data.Title}
-            Year:       ${response.data.Year}
-            Rated:      ${response.data.Rated}
-            Genre:      ${response.data.Genre}
-            Director:   ${response.data.Director}
-            Writer(s):  ${response.data.Writer}
-            Actors:     ${response.data.Actors}
-            Plot:       ${response.data.Plot}
-            ----------------------------------------------------------
+Title:          ${response.data.Title}
+Year:           ${response.data.Year}
+Rated:          ${response.data.Rated}
+Genre:          ${response.data.Genre}
+Director:       ${response.data.Director}
+Writer(s):      ${response.data.Writer}
+Actors:         ${response.data.Actors}
+Plot:           ${response.data.Plot}
+                    ----------------------------------------------------------
             `
                 console.log(dataString);
                 fs.appendFile("log.txt", dataString, function (err) {
@@ -100,22 +94,36 @@ function movieSearch() {
                     else {
                         console.log("Content Added!");
                     }
-
                 });
-
             }
         }
     );
 }
 
-function bandSearch() {
-    axios.get(`rest.bandsintown.com / artists / ${userQuery}`).then(
+function bandSearch(userQuery) {
+    axios.get(`https://rest.bandsintown.com/artists/${userQuery}/events?app_id=codingbootcamp`).then(
         function (response) {
             if (!response) {
                 return console.log('null request');
             }
             else {
-                console.log(response);
+                for (var i = 0; i < response.data.length; i++) {
+
+                    var dataString = `
+Venue:      ${response.data[i].venue.name}
+City:       ${response.data[i].venue.city}
+Date:       ${moment(response.data[i].datetime, ["YYYY", moment.ISO_8601]).format('MM-DD-YYYY hh:mm a')}
+---------------------------------------------------
+                    `
+                    console.log(dataString);
+                    fs.appendFile("log.txt", dataString, function (err) {
+
+                        if (err) {
+                            console.log(err);
+                        }
+
+                    });
+                }
             }
         }
     )
@@ -124,27 +132,26 @@ function bandSearch() {
 function readRandom() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
 
-        // We will then print the contents of data
         console.log(data);
 
-        // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
 
         var request = dataArr[0];
         var query = dataArr[1];
 
-        // We will then re-display the content as an array for later use.
         switch (request) {
             case 'spotify-this-song':
                 songSearch(query);
                 break;
             case 'movie-this':
                 movieSearch(query);
+                break;
+            case 'concert-this':
+                bandSearch(query);
                 break;
             case 'do-what-it-says':
                 read_txt();
